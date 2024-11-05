b6c4f86ed5507e2a3ac04cc5e461d550a667e2ea
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-11-05 17:06:37 +0300

    project css değişti

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -1,105 +1,161 @@
+import "./project.css";
+
 export default function Project() {
   return (
-    <>
-      <div className="projects">
-        <div className="projects">
-          <a
-            href="https://advice-generator-app-hazel-ten.vercel.app/"
-            target="_blank"
-          >
-            advice-generator-app
-          </a>
-          <a
-            href="https://electronic-products-list.vercel.app/"
-            target="_blank"
-          >
-            electronic-products-list
-          </a>
-
-          <a href="https://adres-defteri-kohl.vercel.app/" target="_blank">
-            Adres Defteri
-          </a>
-          <a href="https://alisveris-sepeti-red.vercel.app/" target="_blank">
-            Alışveriş Sepeti
-          </a>
-          <a href="https://tip-calculater-bay.vercel.app/" target="_blank">
-            Bahşiş Hesaplama
-          </a>
-          <a href="https://contact-form-plum-psi.vercel.app/" target="_blank">
-            İletişim Formu
-          </a>
-          <a href="https://countries-two-silk.vercel.app/" target="_blank">
-            countries
-          </a>
-          <a href="https://student-list-mu.vercel.app/" target="_blank">
-            Öğrenci Listesi
-          </a>
-          <a href="https://product-list-app-wine.vercel.app/" target="_blank">
-            Ürün Ekleme Listesi
-          </a>
-          <a href="https://blog-page-app.vercel.app/" target="_blank">
-            Blog Ekleme Sayfası
-          </a>
-          <a href="https://cars-list-gamma.vercel.app/" target="_blank">
-            Araba Listeleme Sayfası
-          </a>
-          <a href="https://gelir-gider-omega.vercel.app/" target="_blank">
-            Gelir-Gider Listesi
-          </a>
-          <a href="https://hesap-makinesi-sage.vercel.app/" target="_blank">
-            Hesap Makinesi-CSS
-          </a>
-          <a href="https://figma-faqs.vercel.app/" target="_blank">
-            Figma Faqs - CSS
-          </a>
-          <a href="https://figma-article.vercel.app/" target="_blank">
-            Figma article - CSS
-          </a>
-          <a href="https://ecommerce-figma.vercel.app/" target="_blank">
-            Figma Ecommerce - CSS
-          </a>
-          <a href="https://website-app-group.vercel.app/">Website App - CSS</a>
-          <a href="https://hero-restaurant-iota.vercel.app/" target="_blank">
-            Figma Hero restaurant - CSS
-          </a>
-          <a href="https://call-to-action-azure.vercel.app/" target="_blank">
-            Figma call to action - CSS
-          </a>
-          <a href="https://hero-sage.vercel.app/" target="_blank">
-            Figma Hero - CSS
-          </a>
-          <a href="https://hero-shop-bice.vercel.app/" target="_blank">
-            Figma Hero Shop - CSS
-          </a>
-          <a href="https://banner-sable.vercel.app/" target="_blank">
-            Figma Banner - CSS
-          </a>
-          <a href="https://hero-saas.vercel.app/" target="_blank">
-            Figma Hero Saas - CSS
-          </a>
-          <a href="https://blog-page-css.vercel.app/" target="_blank">
-            Figma blog page - CSS
-          </a>
-          <a href="https://desing-portfolio.vercel.app/" target="_blank">
-            Figma desing-portfolio - CSS
-          </a>
-          <a href="https://order-summary-lemon-two.vercel.app/">
-            Figma Order Summary - CSS
-          </a>
-          <a href="https://home-page-nine-tau.vercel.app/" target="_blank">
-            Figma Home Page - CSS
-          </a>
-          <a href="https://learning-figma.vercel.app/" target="_blank">
-            Figma Learning Page - CSS
-          </a>
-          <a href="https://typemaster-psi.vercel.app/" target="_blank">
-            Figma typemaster - CSS
-          </a>
-          <a href="https://audiophile-mu-three.vercel.app/" target="_blank">
-            Figma audiophile - CSS
-          </a>
-        </div>
+    <div className="card-container">
+      <div className="card">
+        <a
+          href="https://advice-generator-app-hazel-ten.vercel.app/"
+          target="_blank"
+        >
+          advice-generator-app
+        </a>
       </div>
-    </>
+      <div className="card">
+        <a href="https://electronic-products-list.vercel.app/" target="_blank">
+          electronic-products-list
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://adres-defteri-kohl.vercel.app/" target="_blank">
+          Adres Defteri
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://alisveris-sepeti-red.vercel.app/" target="_blank">
+          Alışveriş Sepeti
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://tip-calculater-bay.vercel.app/" target="_blank">
+          Bahşiş Hesaplama
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://contact-form-plum-psi.vercel.app/" target="_blank">
+          İletişim Formu
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://countries-two-silk.vercel.app/" target="_blank">
+          countries
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://student-list-mu.vercel.app/" target="_blank">
+          Öğrenci Listesi
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://product-list-app-wine.vercel.app/" target="_blank">
+          Ürün Ekleme Listesi
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://blog-page-app.vercel.app/" target="_blank">
+          Blog Ekleme Sayfası
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://cars-list-gamma.vercel.app/" target="_blank">
+          Araba Listeleme Sayfası
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://gelir-gider-omega.vercel.app/" target="_blank">
+          Gelir-Gider Listesi
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://hesap-makinesi-sage.vercel.app/" target="_blank">
+          Hesap Makinesi-CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://figma-faqs.vercel.app/" target="_blank">
+          Figma Faqs - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://figma-article.vercel.app/" target="_blank">
+          Figma article - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://ecommerce-figma.vercel.app/" target="_blank">
+          Figma Ecommerce - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://website-app-group.vercel.app/" target="_blank">
+          Website App - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://hero-restaurant-iota.vercel.app/" target="_blank">
+          Figma Hero restaurant - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://call-to-action-azure.vercel.app/" target="_blank">
+          Figma call to action - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://hero-sage.vercel.app/" target="_blank">
+          Figma Hero - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://hero-shop-bice.vercel.app/" target="_blank">
+          Figma Hero Shop - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://banner-sable.vercel.app/" target="_blank">
+          Figma Banner - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://hero-saas.vercel.app/" target="_blank">
+          Figma Hero Saas - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://blog-page-css.vercel.app/" target="_blank">
+          Figma blog page - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://desing-portfolio.vercel.app/" target="_blank">
+          Figma desing-portfolio - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://order-summary-lemon-two.vercel.app/" target="_blank">
+          Figma Order Summary - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://home-page-nine-tau.vercel.app/" target="_blank">
+          Figma Home Page - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://learning-figma.vercel.app/" target="_blank">
+          Figma Learning Page - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://typemaster-psi.vercel.app/" target="_blank">
+          Figma typemaster - CSS
+        </a>
+      </div>
+      <div className="card">
+        <a href="https://audiophile-mu-three.vercel.app/" target="_blank">
+          Figma audiophile - CSS
+        </a>
+      </div>
+    </div>
   );
 }
