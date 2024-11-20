23b8b5a819d51e2af4e8006481ce649d00e1433d
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-11-20 23:19:44 +0300

    img ekledim ve project kısmını değiştirdim

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import About from "./about/page";
 import PortfolioPage from "./portfolio/page";
 
 export default function Home() {
--- app/project/page.jsx
+++ app/project/page.jsx
@@ -1,161 +1,123 @@
 import "./project.css";
 
-export default function Project() {
+export default function Projects() {
   return (
-    <div className="card-container">
-      <div className="card">
-        <a
-          href="https://advice-generator-app-hazel-ten.vercel.app/"
-          target="_blank"
-        >
-          advice-generator-app
-        </a>
+    <>
+      <div className="container-project">
+        <div className="grid-project">
+          <div className="card-project">
+            <img src={"./img/advice.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a
+                  href="https://advice-generator-app-hazel-ten.vercel.app/"
+                  target="_blank"
+                >
+                  advice-generator-app
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+          <div className="card-project">
+            <img src={"./img/satis.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a
+                  href="https://electronic-products-list.vercel.app/"
+                  target="_blank"
+                >
+                  Electronic-products-list
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+          <div className="card-project">
+            <img src={"./img/learn.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a href="https://learning-figma.vercel.app/" target="_blank">
+                  Figma Learning Page - CSS
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+          <div className="card-project">
+            <img src={"./img/learn.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a
+                  href="https://audiophile-mu-three.vercel.app/"
+                  target="_blank"
+                >
+                  Figma audiophile - CSS
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+          <div className="card-project">
+            <img src={"./img/learn.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a href="https://typemaster-psi.vercel.app/" target="_blank">
+                  Figma typemaster - CSS
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+          <div className="card-project">
+            <img src={"./img/learn.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a
+                  href="https://order-summary-lemon-two.vercel.app/"
+                  target="_blank"
+                >
+                  Figma Order Summary - CSS
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+          <div className="card-project">
+            <img src={"./img/learn.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a href="https://blog-page-css.vercel.app/" target="_blank">
+                  Figma blog page - CSS
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+          <div className="card-project">
+            <img src={"./img/learn.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a href="https://hero-saas.vercel.app/" target="_blank">
+                  Figma Hero Saas - CSS
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+          <div className="card-project">
+            <img src={"./img/learn.png"} alt="" />
+            <div className="card-project-content">
+              <h3 className="card-project-title">
+                <a href="https://banner-sable.vercel.app/" target="_blank">
+                  Figma Banner - CSS
+                </a>
+              </h3>
+              <p className="card-project-category">category</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="card">
-        <a href="https://electronic-products-list.vercel.app/" target="_blank">
-          electronic-products-list
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://adres-defteri-kohl.vercel.app/" target="_blank">
-          Adres Defteri
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://alisveris-sepeti-red.vercel.app/" target="_blank">
-          Alışveriş Sepeti
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://tip-calculater-bay.vercel.app/" target="_blank">
-          Bahşiş Hesaplama
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://contact-form-plum-psi.vercel.app/" target="_blank">
-          İletişim Formu
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://countries-two-silk.vercel.app/" target="_blank">
-          countries
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://student-list-mu.vercel.app/" target="_blank">
-          Öğrenci Listesi
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://product-list-app-wine.vercel.app/" target="_blank">
-          Ürün Ekleme Listesi
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://blog-page-app.vercel.app/" target="_blank">
-          Blog Ekleme Sayfası
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://cars-list-gamma.vercel.app/" target="_blank">
-          Araba Listeleme Sayfası
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://gelir-gider-omega.vercel.app/" target="_blank">
-          Gelir-Gider Listesi
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://hesap-makinesi-sage.vercel.app/" target="_blank">
-          Hesap Makinesi-CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://figma-faqs.vercel.app/" target="_blank">
-          Figma Faqs - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://figma-article.vercel.app/" target="_blank">
-          Figma article - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://ecommerce-figma.vercel.app/" target="_blank">
-          Figma Ecommerce - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://website-app-group.vercel.app/" target="_blank">
-          Website App - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://hero-restaurant-iota.vercel.app/" target="_blank">
-          Figma Hero restaurant - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://call-to-action-azure.vercel.app/" target="_blank">
-          Figma call to action - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://hero-sage.vercel.app/" target="_blank">
-          Figma Hero - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://hero-shop-bice.vercel.app/" target="_blank">
-          Figma Hero Shop - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://banner-sable.vercel.app/" target="_blank">
-          Figma Banner - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://hero-saas.vercel.app/" target="_blank">
-          Figma Hero Saas - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://blog-page-css.vercel.app/" target="_blank">
-          Figma blog page - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://desing-portfolio.vercel.app/" target="_blank">
-          Figma desing-portfolio - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://order-summary-lemon-two.vercel.app/" target="_blank">
-          Figma Order Summary - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://home-page-nine-tau.vercel.app/" target="_blank">
-          Figma Home Page - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://learning-figma.vercel.app/" target="_blank">
-          Figma Learning Page - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://typemaster-psi.vercel.app/" target="_blank">
-          Figma typemaster - CSS
-        </a>
-      </div>
-      <div className="card">
-        <a href="https://audiophile-mu-three.vercel.app/" target="_blank">
-          Figma audiophile - CSS
-        </a>
-      </div>
-    </div>
+    </>
   );
 }
