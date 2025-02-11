78efc23cc9ba2aa37e3797664fc381b1f83f553b
Sema Bekdemir <ecesemadimen96@gmail.com>
2025-02-11 22:31:11 +0300

    ana sayfadaki öne çıkan projeleri ayarladım

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,7 +1,30 @@
 import Skills from "@/components/skills";
 import Image from "next/image";
 import Hero from "@/components/hero";
-import Contact from "@/components/contact";
+import Link from "next/link";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Linkedln Clone",
+    description:
+      "A professional network platform that replicates core LinkedIn features, allowing users to connect, share updates, and manage their profiles.",
+    image: "/img/linkedln.png",
+    category: "Full Stack",
+    githubLink: "https://github.com/ecesemabekdemir/linkedln-clone",
+    liveLink: "https://linkedln-clone-umber.vercel.app/linked",
+  },
+  {
+    id: 2,
+    title: "Website App",
+    description:
+      "A professional network platform built with React and Bootstrap, featuring a customizable dark mode and a user-friendly dashboard for managing profiles and connecting with others.",
+    image: "/img/websiteapp.jpeg",
+    category: "CSS",
+    githubLink: "https://github.com/ecesemabekdemir/websiteApp",
+    liveLink: "https://website-app-sandy.vercel.app/",
+  },
+];
 
 export default function Home() {
   return (
@@ -13,35 +36,31 @@ export default function Home() {
         <div className="container">
           <h2 className="section-title">Featured Projects</h2>
           <div className="projects-grid">
-            {[1, 2, 3].map((project) => (
+            {projectsData.map((project) => (
               <div key={project} className="project-card">
                 <Image
-                  src="/img/learn.png"
+                  src={project.image}
                   alt={`Project ${project}`}
                   width={300}
                   height={200}
                   className="project-image"
                 />
                 <div className="project-content">
-                  <h3 className="project-title">Project {project}</h3>
-                  <p className="project-description">
-                    A modern web application built with React and Next.js,
-                    featuring responsive design and smooth animations.
-                  </p>
-                  <a
-                    href="https://banner-sable.vercel.app/"
+                  <h3 className="project-title">{project.title}</h3>
+                  <p className="project-description">{project.description}</p>
+                  <Link
+                    href={project.liveLink}
                     target="_blank"
                     className="project-link"
                   >
                     View Project
-                  </a>
+                  </Link>
                 </div>
               </div>
             ))}
           </div>
         </div>
       </div>
-
       <Skills />
     </div>
   );
