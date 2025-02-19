806cbf7a261d16fd9396074165f8557706f6be21
Sema Bekdemir <ecesemadimen96@gmail.com>
2025-02-19 13:41:13 +0300

    nav mobile buraya yazıldı

--- components/nav/index.jsx
+++ components/nav/index.jsx
@@ -1,31 +1,34 @@
 import Link from "next/link";
 import "./nav.css";
-import MobileNav from "./mobile";
+
 export default function Nav() {
   return (
-    <>
-      <div className="nav">
-        <div className="container nav-content">
-          <Link href="/" className="nav-logo">
-            SEMA BEKDEMIR
-          </Link>
-          <ul className="nav-links">
-            <li>
-              <Link href="/home">Home</Link>
-            </li>
-            <li>
-              <Link href="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="#contact">Contact</Link>
-            </li>
-          </ul>
-          <MobileNav />
-        </div>
+    <div className="nav-container">
+      <div className="nav-content">
+        <Link href="/" className="nav-logo">
+          SEMA BEKDEMIR
+        </Link>
+        <input type="checkbox" id="menu-toggle" />
+        <label for="menu-toggle" class="menu-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+        <ul className="nav-links">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
